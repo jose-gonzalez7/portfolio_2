c4b03297c4825a1fca80302bb3e531c3f924df5d
Jose Gonzalez <jgonzalezroman7@gmail.com>
2026-01-06 01:49:58 +0100

    cambiado fondos

--- portfolio/src/componentes/secciones/Educacion.tsx
+++ portfolio/src/componentes/secciones/Educacion.tsx
@@ -1,5 +1,5 @@
 import React, { useRef, useState } from 'react';
-import { motion, useScroll, useTransform } from "framer-motion";
+import { motion} from "framer-motion";
 import { 
   ShieldCheck, 
   Globe, 
